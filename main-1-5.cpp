aa283b8a8f84b55bb7c1258cf6fe7b5c3e583f30
michellenguyen <michellenguyen@student.adelaide.edu.au>
2024-03-01 21:46:10 +1030

    1-5

--- main-1-5.cpp
+++ main-1-5.cpp
@@ -4,7 +4,6 @@ using namespace std;
 extern int count_evens_from_1(int number);
 
 int main() {
-    int number;
     cout << "The number of even numbers from 1 is: " << count_evens_from_1(4) << endl;
     return 0;
 }
--- main-3-1.cpp
+++ main-3-1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 extern bool is_fanarray(int array[], int n);
 
 int main() {
-    int array[7] = {1,2,3,5,3,2,1};
+    int array[7] = {1,2,3,3,3,2,1};
     string answer = (is_fanarray(array,7)) ? "true" : "false";
     cout << "The array is a fan, true or false: " << answer << endl;
     return 0;
