a1a8e9d19740c1b7057354e9e48d0e59ab65251c
michellenguyen <michellenguyen@student.adelaide.edu.au>
2024-03-01 22:10:31 +1030

    3-1

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
-int count_evens(int number){
+/*
+Note: you can change the names of the *function parameters* if you wish, but NOT THE FUNCTION NAME. Gradescope will mark it wrong.
+They are only used within their own functions.
+
+What are the function parameters?
+Eg.
+void functionName(parameter1, parameter2, parameter3) {
+  // code to be executed
+}
+*/
+int count_evens(int number){ //int number is a function parameter here
     int count=0;
     if (number>=1){
         for(int i=1; i<=number; i++){
--- main-3-1.cpp
+++ main-3-1.cpp
@@ -4,8 +4,8 @@ using namespace std;
 extern bool is_fanarray(int array[], int n);
 
 int main() {
-    int array[7] = {1,2,3,3,3,2,1};
-    string answer = (is_fanarray(array,7)) ? "true" : "false";
+    int array[8] = {1,2,5,5,5,5,2,1};
+    string answer = (is_fanarray(array,8)) ? "true" : "false";
     cout << "The array is a fan, true or false: " << answer << endl;
     return 0;
 }
