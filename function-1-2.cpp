b535729900e8d3dc3a29d7a756a3fd6e177b82a8
michellenguyen <michellenguyen@student.adelaide.edu.au>
2024-03-08 11:08:22 +1030

    update 1-2

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+using namespace std;
 
 int is_identity(int array[10][10])
 {
     int result=1; //in the beginning, true
     for(int i=0;i<10;i++)
     {
-        for(int j=0; i<10; j++)
+        for(int j=0; j<10; j++)
         {
             if(i==j)
             {
@@ -23,6 +24,7 @@ int is_identity(int array[10][10])
             }
         }
     }
+    
     return result;
 
 }
--- main-1-2.cpp
+++ main-1-2.cpp
@@ -14,6 +14,6 @@ int main() {
                         {21,22,23,24,25,26,27,28,29,30},
                         {31,32,33,34,35,36,37,38,39,40},
                         {41,42,43,44,45,46,47,48,49,50}};
-    cout << "if the matrix is an identity matrix or not. 1 if it is an identity matrix and a 0 if not.: " << is_identity(array) << endl;
+    cout << "if the matrix is an identity matrix or not. 1 if it is an identity matrix and a 0 if not.: " <<is_identity(array) << endl;
     return 0;
 }
