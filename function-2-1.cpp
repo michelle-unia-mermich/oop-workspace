f8bb450dc4437cb7a8a64ab37207405ffc1d3076
michellenguyen <michellenguyen@student.adelaide.edu.au>
2024-03-08 13:55:40 +1030

    2-1

---
+++ function-2-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void print_binary_str(string decimal_str)
+{
+    int decimal=stoi(decimal_str);//convert string to integer
+    long binary=0;
+
+    cout<<binary<<endl;
+}
