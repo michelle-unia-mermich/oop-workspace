8cf50337ab4d2ba5026543459f61692756594abf
michellenguyen1909 <michellehanng@gmail.com>
2024-04-30 14:53:07 +0930

    u

---
+++ Grade.cpp
@@ -0,0 +1,20 @@
+#include "Grade.h"
+
+#include <iostream>
+using namespace std;
+
+Grade::Grade()
+{ 
+    assignment="";
+    value=0;
+}
+
+Grade::Grade(string _assignment, int _value)
+{
+    id=input_id;
+}
+
+int Grade::get_id()
+{
+    return id;
+}
---
+++ Student.cpp
@@ -0,0 +1,14 @@
+#include "Person.h"
+#include "Student.h"
+#include <iostream>
+using namespace std;
+
+Student::Student():Person()
+{ 
+    id=0;
+}
+
+Student::Student(string input_name, int input_id):Person(input_name)
+{
+    id=input_id;
+}
