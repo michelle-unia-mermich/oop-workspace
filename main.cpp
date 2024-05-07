1d0b43248fc425e60f99ef365b927599161e0229
michelle-unia-mermich <michelle.nguyen@student.adelaide.edu.au>
2024-05-07 15:40:13 +0930

    update

--- main.cpp
+++ main.cpp
@@ -20,16 +20,17 @@ int main()
 
     //3-Add a student to this course of the uni
     Student student1= Student("Michelle",1894969);
-    Student * student1_pointer= &student1; //can also do this dynamic array
-    uni.get_Course_pointer()[0].addPerson(student1_pointer);
 
     //4-Add a grade for this student for *some* of the assignments
     //Add Grade to the Gradebook of the university
     uni.get_Gradebook().addGrade(1894969, 1203, "Practical 1", 100); 
+    cout<<uni.get_Gradebook().get_count()<<endl;
     //WHEN I ADD LIKE THIS uni.get_Gradebook() returns the element inside the object Gradebook inside the object umi. 
     //But does making to this returned object also make change to the object in the uni object of University class? Since we not chaningn the address the poitner points to.
     uni.get_Gradebook().addGrade(1894969, 1203, "Practical 2", 50);
+    cout<<uni.get_Gradebook().get_count()<<endl;
     uni.get_Gradebook().addGrade(1894969, 1203, "Workshop 1", 100);
+    cout<<uni.get_Gradebook().get_count()<<endl;
 
     
     //print out the elements in array of Grade objects in Gradebook contained in University
@@ -38,14 +39,7 @@ int main()
         cout << uni.get_Gradebook().get_Grade_inGradebook()[i].get_value() << endl;
         cout << uni.get_Gradebook().get_Grade_inGradebook()[i].get_assignment() << endl;
     }
-    //THIS ONE DOES NOT WORK. SEE test.cpp, which works perfectly
-
-    //print out the address of the Gradebook object taken from uni.get_Gradebook();
-    //1- the adress of the Gradebook printed out by get_Gradebook()
-    Gradebook* pointer1=&(uni.get_Gradebook()); //error: taking the address of a temporary object of type 'Gradebook' [-Waddress-of-temporary]
-    //2- the address of Gradebook object taken from the function get_pointerGradebook()
-    Gradebook* pointer2=uni.get_pointerGradebook();
-    cout<< "Compare the two pointers, pointer 1 "<< pointer1 <<" and pointer 2: "<<pointer2<<endl;
+    //THIS ONE DOES NOT WORK. SEE test.cpp, which works perfectl
 
 
     return 0;
