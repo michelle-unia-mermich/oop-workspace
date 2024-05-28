deee8dbecb854c7fe225f4ead61808dac9cf602c
michelle-unia-mermich <michelle.nguyen@student.adelaide.edu.au>
2024-05-28 17:29:15 +0930

    u

--- main-1.cpp
+++ main-1.cpp
@@ -8,7 +8,11 @@ int main()
     Utils utilsObject;
     std::tuple<int, int> randomPosition1= utilsObject.generateRandomPos(960, 640);
     std::tuple<int, int> randomPosition2= utilsObject.generateRandomPos(960, 640);
-    std::cout<< "The distance is "<< utilsObject.calculateDistance(randomPosition1, randomPosition2)<<std::endl;
+    double distance = utilsObject.calculateDistance(randomPosition1, randomPosition2);
+    std::cout<< "The distance is "<< distance <<std::endl;
 
+    //cannot call member function ‘double Utils::calculateDistance(std::tuple<int, int>, std::tuple<int, int>)’ without object
+    //double distance = Utils::calculateDistance(pos1, pos2);
+    //therefore I add 
     return 0;
 }
